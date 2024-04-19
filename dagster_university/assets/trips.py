5228e4f86ab695f9482ec76a79a83cafffb4e0b3
Jay Zern Ng <jayzern@hotmail.com>
2024-04-19 15:17:11 -0400

    add sensors, metadata and groupings

--- dagster_university/assets/trips.py
+++ dagster_university/assets/trips.py
@@ -3,23 +3,24 @@ import os
 
 import requests
 from . import constants
-from dagster import asset
+from dagster import asset, MetadataValue, MaterializeResult
 from datetime import datetime, timedelta
 import pandas as pd
 from dagster_duckdb import DuckDBResource
-from ..partitions import monthly_partition
+from ..partitions import monthly_partition, weekly_partition
 
 
 @asset(
-    partitions_def=monthly_partition
+    partitions_def=monthly_partition,
+    group_name="raw_files"
 )
 def taxi_trips_file(context):
     """
       The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal.
     """
+
     partition_date_str = context.asset_partition_key_for_output()
     month_to_fetch = partition_date_str[:-3]
-    # month_to_fetch = '2023-03'
 
     raw_trips = requests.get(
         f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
@@ -28,8 +29,18 @@ def taxi_trips_file(context):
     with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
         output_file.write(raw_trips.content)
 
+    num_rows = len(pd.read_parquet(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch)))
 
-@asset
+    return MaterializeResult(
+        metadata={
+            'Number of records': MetadataValue.int(num_rows)
+        }
+    )
+
+
+@asset(
+    group_name="raw_files",
+)
 def taxi_zones_file():
     """
       The raw CSV file for the taxi zones dataset. Sourced from the NYC Open Data portal.
@@ -43,7 +54,8 @@ def taxi_zones_file():
 
 @asset(
     deps=["taxi_trips_file"],
-    partitions_def=monthly_partition
+    partitions_def=monthly_partition,
+    group_name="ingested"
 )
 def taxi_trips(context, database: DuckDBResource):
     """
@@ -74,64 +86,72 @@ def taxi_trips(context, database: DuckDBResource):
 
 
 @asset(
-    deps=["taxi_zones_file"]
+    deps=["taxi_zones_file"],
+    group_name="ingested"
 )
 def taxi_zones(database: DuckDBResource):
-    sql_query = """
+    """
+        The raw taxi zones dataset, loaded into a DuckDB database.
+    """
+
+    query = f"""
         create or replace table zones as (
-          select
-            LocationID AS zone_id,
-            zone,
-            borough,
-            the_geom AS geometry
-          from '{constants.TAXI_ZONES_FILE_PATH}'
+            select
+                LocationID as zone_id,
+                zone,
+                borough,
+                the_geom as geometry
+            from '{constants.TAXI_ZONES_FILE_PATH}'
         );
     """
+
     with database.get_connection() as conn:
-        conn.execute(sql_query)
+        conn.execute(query)
 
 
-@asset(deps=["taxi_trips"])
-def trips_by_week(database: DuckDBResource):
-	# conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
+@asset(
+    deps=["taxi_trips"],
+    partitions_def=weekly_partition,
+    group_name="metrics"
+)
+def trips_by_week(context, database: DuckDBResource):
+    """
+      The number of trips per week, aggregated by week.
+    """
 
-	current_date = datetime.strptime("2023-03-01", constants.DATE_FORMAT)
-	end_date = datetime.strptime("2023-04-01", constants.DATE_FORMAT)
+    period_to_fetch = context.asset_partition_key_for_output()
 
-	result = pd.DataFrame()
-     
-	while current_date < end_date:
-		current_date_str = current_date.strftime(constants.DATE_FORMAT)
-		query = f"""
-			select
-				vendor_id, total_amount, trip_distance, passenger_count
-			from trips
-			where date_trunc('week', pickup_datetime) = date_trunc('week', '{current_date_str}'::date)
-		"""
+    # get all trips for the week
+    query = f"""
+        select vendor_id, total_amount, trip_distance, passenger_count
+        from trips
+        where pickup_datetime >= '{period_to_fetch}'
+            and pickup_datetime < '{period_to_fetch}'::date + interval '1 week'
+    """
 
-		# data_for_week = conn.execute(query).fetch_df()
-		with database.get_connection() as conn:
-			data_for_week = conn.execute(query).fetch_df()
+    with database.get_connection() as conn:
+        data_for_month = conn.execute(query).fetch_df()
 
-		aggregate = data_for_week.agg({
-			"vendor_id": "count",
-			"total_amount": "sum",
-			"trip_distance": "sum",
-			"passenger_count": "sum"
-		}).rename({"vendor_id": "num_trips"}).to_frame().T # type: ignore
+    aggregate = data_for_month.agg({
+        "vendor_id": "count",
+        "total_amount": "sum",
+        "trip_distance": "sum",
+        "passenger_count": "sum"
+    }).rename({"vendor_id": "num_trips"}).to_frame().T # type: ignore
 
-		aggregate["period"] = current_date
+    # clean up the formatting of the dataframe
+    aggregate["period"] = period_to_fetch
+    aggregate['num_trips'] = aggregate['num_trips'].astype(int)
+    aggregate['passenger_count'] = aggregate['passenger_count'].astype(int)
+    aggregate['total_amount'] = aggregate['total_amount'].round(2).astype(float)
+    aggregate['trip_distance'] = aggregate['trip_distance'].round(2).astype(float)
+    aggregate = aggregate[["period", "num_trips", "total_amount", "trip_distance", "passenger_count"]]
 
-		result = pd.concat([result, aggregate])
-
-		current_date += timedelta(days=7)
-
-	# clean up the formatting of the dataframe
-	result['num_trips'] = result['num_trips'].astype(int)
-	result['passenger_count'] = result['passenger_count'].astype(int)
-	result['total_amount'] = result['total_amount'].round(2).astype(float)
-	result['trip_distance'] = result['trip_distance'].round(2).astype(float)
-	result = result[["period", "num_trips", "total_amount", "trip_distance", "passenger_count"]]
-	result = result.sort_values(by="period")
-
-	result.to_csv(constants.TRIPS_BY_WEEK_FILE_PATH, index=False)
+    try:
+        # If the file already exists, append to it, but replace the existing month's data
+        existing = pd.read_csv(constants.TRIPS_BY_WEEK_FILE_PATH)
+        existing = existing[existing["period"] != period_to_fetch]
+        existing = pd.concat([existing, aggregate]).sort_values(by="period")
+        existing.to_csv(constants.TRIPS_BY_WEEK_FILE_PATH, index=False)
+    except FileNotFoundError:
+        aggregate.to_csv(constants.TRIPS_BY_WEEK_FILE_PATH, index=False)
